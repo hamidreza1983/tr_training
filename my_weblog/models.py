beb3098edad54592a47bd2a60be6e66a681287b0
hamidreza1983 <126154017+hamidreza1983@users.noreply.github.com>
2023-03-15 04:37:03 +0330

    create model
    + model post
    + model contact

--- my_weblog/models.py
+++ my_weblog/models.py
@@ -13,3 +13,28 @@ class post(models.Model):
     published_date = models.DateTimeField(null=True)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ['-created_date']
+
+    def __str__(self):
+        return self.title
+
+
+class contact(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    subject = models.CharField(max_length=255)
+    text = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ['created_date']
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
+    
