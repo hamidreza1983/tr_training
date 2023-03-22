592415c333e9c0aa7f6d4b72476954611a1ed5e9
hamid reza <126154017+hamidreza1983@users.noreply.github.com>
2023-03-23 02:53:38 +0430

    next prev func
    + functionality of next and prev

--- blog/views.py
+++ blog/views.py
@@ -12,11 +12,24 @@ def b_home(req):
     return render(req,'blog/blog-home.html', context=context)
 
 def b_single(req, pid):
+    total = post.objects.all()
     posts=get_object_or_404(post, pk=pid, status=1)
     posts.counted_viwes += 1
     posts.save()
+    if pid == len(total):
+        prev = posts.get_object_or_404(post, pk=pid-1, status=1)
+        next = None
+    elif pid == 1 :
+        prev = None
+        next = posts.get_object_or_404(post, pk=pid+1, status=1)
+    else:
+        prev = posts.get_object_or_404(post, pk=pid-1, status=1)
+        next = posts.get_object_or_404(post, pk=pid+1, status=1)
+
     context = {
-        'post':posts
+        'post' : posts,
+        'next' : next,
+        'prev' : prev,
     }
     return render(req,'blog/blog-single.html', context=context)
 
