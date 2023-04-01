878295b7364e186d554264ac63affb2242544e1f
hamidreza1983 <126154017+hamidreza1983@users.noreply.github.com>
2023-04-01 23:59:39 +0200

    create massage
    add message

--- my_weblog/views.py
+++ my_weblog/views.py
@@ -3,12 +3,17 @@ from .models import cheap_package, luxary_package, camping_package, newsletter
 from django.utils import timezone
 from .models import contact
 from .forms import ContactForm, NewsletterForm
+from django.contrib import messages
 
 def home(req):
     if req.method == 'POST':
         email = NewsletterForm(req.POST)
         if email.is_valid():
            email.save()
+           messages.add_message(req, messages.SUCCESS, 'thankyou for register')
+        else:
+            messages.add_message(req, messages.ERROR, 'your email is invalid')
+
     cheap = cheap_package.objects.all()
     luxary= luxary_package.objects.all()
     camp= camping_package.objects.all()
@@ -23,8 +28,12 @@ def about(req):
     return render(req,'my_weblog/about.html')
 
 def Contact(req):
+    
     if req.method == 'POST':
         form = ContactForm(req.POST)
         if form.is_valid():
             form.save()
+            messages.add_message(req, messages.SUCCESS, 'your message received')
+        else:
+            messages.add_message(req, messages.ERROR, 'your data is not enough')
     return render(req,'my_weblog/contact.html')
