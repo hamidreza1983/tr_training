ab3dc8699744a27f65e10e3aeab23570f988ac0d
hamidreza1983 <126154017+hamidreza1983@users.noreply.github.com>
2023-04-04 04:01:51 +0200

    create comment post
    + add comment model

--- blog/models.py
+++ blog/models.py
@@ -29,3 +29,20 @@ class post(models.Model):
 
     def snipets(self):
         return self.content[:20] + '...'
+
+
+class comment(models.Model):
+    post = models.ForeignKey(post, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    subject = models.CharField(max_length=255)
+    text = models.TextField()
+    status = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+
+        ordering = ['-created_date']
+    def __str__(self):
+        return self.name
