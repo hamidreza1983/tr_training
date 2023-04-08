b5cbe4cc23c5092b11fbaed442cbc7bc5e8523f8
hamidreza1983 <126154017+hamidreza1983@users.noreply.github.com>
2023-04-08 14:30:40 +0200

    update login
    login with django authenticate form

--- login/views.py
+++ login/views.py
@@ -1,21 +1,32 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from blog.models import post
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
 
-def login(req):
-    if req.method == 'POST':
-        username = req.POST['username']
-        password = req.POST['password']
-        print (username)
-        print (password)
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            posts = post.objects.filter(status=1)
-            return render(req,'blog/blog-home.html', {'posts':posts})
-        else:
-            return render(req,'my_weblog/index.html')
-    return render(req,'login/login.html')
-    
+def Login(req):
+    if not req.user.is_authenticated:
+        if req.method == 'POST':
+            form = AuthenticationForm(req, req.POST)
+            if form.is_valid():
+                user = form.cleaned_data.get('username')
+                password = form.cleaned_data.get('password')
+                user = authenticate(req,username=user,password=password)
+                print(user)
+                if user is not None:
+                    login(req,user)
+                    return redirect('/blog')
+                else:
+                    messages.add_message(req, messages.WARNING, 'not found this username')
+            else:
+                messages.add_message(req, messages.ERROR, 'input date not possible')
+        form = AuthenticationForm()
+        context = {
+            'form':form,
+        }
+        return render(req,'login/login.html', context=context)
+    else:
+        redirect(req,'/')
     
 
     
